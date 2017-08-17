8f2d9a2cf2d21c0e9f356e2d1d6760f01174b905
Felix <felax@raa.se>
2017-08-17 16:41:04 +0200

    PLRSB-648 Har gjort så att man inte kan tabba utanför modal och dialog, samt butt namn från ng2 till ng.. mm

--- src/components/raa-dialogue/raa-dialogue.component.ts
+++ src/components/raa-dialogue/raa-dialogue.component.ts
@@ -1,39 +1,66 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, AfterViewInit, Input, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'raa-dialogue',
     templateUrl: './raa-dialogue.component.html',
     styleUrls: ['./raa-dialogue.component.scss']
 })
-export class RaaDialogueComponent implements OnInit {
+export class RaaDialogueComponent implements AfterViewInit {
 
-    @Input()
-    displayDialogue: boolean;
+  @Input()
+  onYes: Function;
 
-    @Input()
-    onYes: Function;
+  @Input()
+  onNo: Function;
 
-    @Input()
-    onNo: Function;
+  @Input()
+  onOk: Function;
 
-    @Input()
-    onOk: Function;
+  @Input()
+  onCancel: Function;
 
-    @Input()
-    onCancel: Function;
+  @Input()
+  onClose: Function;
 
-    @Input()
-    onClose: Function;
+  @Input()
+  header: string;
 
-    @Input()
-    header: string;
+  constructor(
+    private element: ElementRef
+  ) { }
 
-    constructor() { }
+  ngAfterViewInit(): void {
 
-    ngOnInit(): void {
-         if (!this.header) {
-            throw 'ERROR: raa-dialogue.component -> header must be specified (string)';
-        }
+    if (!this.header) {
+      throw 'ERROR: raa-dialogue.component -> header must be specified (string)';
     }
+
+    let tabbableElements = Array.prototype.slice.call(this.element.nativeElement.querySelectorAll('select, input, textarea, button, a'))
+    .filter((el: HTMLElement) => !el.hidden);
+
+    if (tabbableElements && tabbableElements.length) {
+
+        let first = tabbableElements[0];
+        let last = tabbableElements[tabbableElements.length - 1];
+
+        let buttonToFocus = this.onClose ? 1 : 0;
+
+        tabbableElements[buttonToFocus].focus();
+
+        last.addEventListener('keydown', function (e: KeyboardEvent) {
+            if ((e.which === 9 && !e.shiftKey)) {
+                e.preventDefault();
+                first.focus();
+            }
+        });
+
+        first.addEventListener('keydown', function (e: KeyboardEvent) {
+            if ((e.which === 9 && e.shiftKey)) {
+                e.preventDefault();
+                last.focus();
+            }
+        });
+    }
+  }
 }
 
--- src/components/raa-modal-page/raa-modal-page.component.ts
+++ src/components/raa-modal-page/raa-modal-page.component.ts
@@ -1,20 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'raa-modal-page',
     templateUrl: './raa-modal-page.component.html',
     styleUrls: ['./raa-modal-page.component.scss']
 })
-export class RaaModalPageComponent {
+export class RaaModalPageComponent implements AfterViewInit {
 
-    @Input()
+  @Input()
     onClose: Function;
 
-    @Input()
-    header: string;
+  @Input()
+  header: string;
 
-    @Input()
-    size: 'small' | 'large';
+  @Input()
+  size: 'small' | 'large';
+
+  constructor (
+    private element: ElementRef
+  ) {}
+
+  ngAfterViewInit() {
+
+    let tabbableElements = Array.prototype.slice.call(this.element.nativeElement.querySelectorAll('select, input, textarea, button, a'))
+        .filter((el: HTMLElement) => !el.hidden);
+
+    if (tabbableElements && tabbableElements.length) {
+
+        let first = tabbableElements[0];
+        let last = tabbableElements[tabbableElements.length - 1];
+
+        let buttonToFocus = this.onClose ? 1 : 0;
+
+        tabbableElements[buttonToFocus].focus();
+
+        last.addEventListener('keydown', function (e: KeyboardEvent) {
+            if ((e.which === 9 && !e.shiftKey)) {
+                e.preventDefault();
+                first.focus();
+            }
+        });
+
+        first.addEventListener('keydown', function (e: KeyboardEvent) {
+            if ((e.which === 9 && e.shiftKey)) {
+                e.preventDefault();
+                last.focus();
+            }
+        });
+    }
+  }
 
 }
 
