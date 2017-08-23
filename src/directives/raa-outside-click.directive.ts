25f191151089619630ec377613fd2b9007d23a66
Emil Klasson <emil.klasson@raa.se>
2017-08-23 14:29:16 +0200

    Ändrat så vi lyssnar på mouseup istället för klick

--- src/directives/raa-outside-click.directive.ts
+++ src/directives/raa-outside-click.directive.ts
@@ -10,7 +10,9 @@ import {
   selector: '[raaOutsideClick]'
 })
 export class OutsideClickDirective {
-  @HostListener('document:click', ['$event'])
+  // Vi måste lyssna på mouseup eller mousedown istället för click. Vid klick så kan element ha tagit borts från DOM:en och då ge ett
+  // felaktigt "utanförklick".
+  @HostListener('document:mouseup', ['$event'])
   onClick(event: MouseEvent) {
     this.handleClick(event);
   }
@@ -18,28 +20,16 @@ export class OutsideClickDirective {
   @Output('raaOutsideClick')
   onOutsideClick = new EventEmitter<void>();
 
-  private firstTime = true;
-
   constructor(
     private elementRef: ElementRef
   ) { }
 
   private handleClick(event: MouseEvent) {
-    if (!this.firstTime) {
       const containerElement = this.elementRef.nativeElement as HTMLElement;
       const targetExistsInContainer = containerElement.contains(event.target as Node);
 
-      // Om det klickade elementet inte finns i containerElement och existerar i DOM:en har användaren klickat utanför.
-      // Vi måste kontrollera att elementet finns i DOM:en för att ta hänsyn till att element kan tas bort vid klick inne i containerElement
-      if (!targetExistsInContainer && this.existsInDOM((event.target))) {
+      if (!targetExistsInContainer) {
         this.onOutsideClick.emit();
       }
-    } else {
-      this.firstTime = false;
-    }
-  }
-
-  private existsInDOM(element: EventTarget) {
-    return document.body.contains(element as Node);
   }
 }
