43fa818d4f228ba96a006115b71a72db4ee453ed
Emil Klasson <emil.klasson@raa.se>
2017-08-24 16:57:46 +0200

    Visar alla värden när användarne öppnar dropdown:en samt markerar hela text fältet alltid istället för att bara göra det på focus

--- src/components/raa-select/raa-select.component.ts
+++ src/components/raa-select/raa-select.component.ts
@@ -67,6 +67,9 @@ export class RaaSelect implements OnInit, OnChanges, AfterViewInit, ControlValue
 
   @Output() onSelect = new EventEmitter<any>();
 
+  @ViewChild('inputField')
+  inputField: ElementRef;
+
   @ViewChild('dropdown')
   dropdown: ElementRef;
 
@@ -164,9 +167,10 @@ export class RaaSelect implements OnInit, OnChanges, AfterViewInit, ControlValue
 
   openDropdownIfClosed() {
     if (!this.showDropdown) {
-      this.filterValues();
+      this.clearFilters();
       this.setHoverIndexFromSelectedValue();
       this.scrollToSelected = true;
+      this.selectAllTextInInput();
 
       return this.showDropdown = true;
     }
@@ -324,6 +328,9 @@ export class RaaSelect implements OnInit, OnChanges, AfterViewInit, ControlValue
   // }
 
 
+  private selectAllTextInInput() {
+    this.inputField.nativeElement.select();
+  }
 }
 
 export type DomainValue = { id: any; displayValue: string };
