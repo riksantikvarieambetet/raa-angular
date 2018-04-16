44f1785b1ff4665071229408ab655ab9955b23ab
Axel Olsson <axel.olsson@gmail.com>
2018-04-16 11:37:41 +0200

    Exporterar allt

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,22 +1,22 @@
 export * from './raa.module';
 
 // Components
-import { RaaSelect } from './components/raa-select/raa-select.component';
-import { RaaDropdownComponent } from './components/raa-dropdown/raa-dropdown.component';
-import { RaaModalPageComponent } from './components/raa-modal-page/raa-modal-page.component';
-import { RaaDialogueComponent } from './components/raa-dialogue/raa-dialogue.component';
-import { RaaLabel } from './components/raa-label/raa-label.component';
-import { RaaTextWithSpinner } from './components/raa-text-with-spinner/raa-text-with-spinner.component';
-import { RaaSpinnerComponent } from './components/raa-spinner/raa-spinner.component';
-import { RaaOverlaySpinnerComponent } from './components/raa-overlay-spinner/raa-overlay-spinner.component';
-import { RaaPingServiceComponent } from './components/raa-ping-service/raa-ping-service.component';
+export { RaaSelect } from './components/raa-select/raa-select.component';
+export { RaaDropdownComponent } from './components/raa-dropdown/raa-dropdown.component';
+export { RaaModalPageComponent } from './components/raa-modal-page/raa-modal-page.component';
+export { RaaDialogueComponent } from './components/raa-dialogue/raa-dialogue.component';
+export { RaaLabel } from './components/raa-label/raa-label.component';
+export { RaaTextWithSpinner } from './components/raa-text-with-spinner/raa-text-with-spinner.component';
+export { RaaSpinnerComponent } from './components/raa-spinner/raa-spinner.component';
+export { RaaOverlaySpinnerComponent } from './components/raa-overlay-spinner/raa-overlay-spinner.component';
+export { RaaPingServiceComponent } from './components/raa-ping-service/raa-ping-service.component';
 
 // Directives
-import { OutsideClickDirective } from './directives/raa-outside-click.directive';
-import { RaaRequestFocusDirective } from './directives/raa-request-focus.directive';
-import { RaaSelectAllOnFocus } from './directives/raa-select-all-on-focus.directive';
-import { RaaFocusInvalidElementDirective } from './directives/raa-focus-invalid-element.directive';
-import { RaaTrapFocusDirective } from './directives/raa-trap-focus.directive';
+export { OutsideClickDirective } from './directives/raa-outside-click.directive';
+export { RaaRequestFocusDirective } from './directives/raa-request-focus.directive';
+export { RaaSelectAllOnFocus } from './directives/raa-select-all-on-focus.directive';
+export { RaaFocusInvalidElementDirective } from './directives/raa-focus-invalid-element.directive';
+export { RaaTrapFocusDirective } from './directives/raa-trap-focus.directive';
 
 // Pipes
-import { OrgnummerPipe } from './pipes/orgnummer.pipe';
+export { OrgnummerPipe } from './pipes/orgnummer.pipe';
