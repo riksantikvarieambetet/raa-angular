4f83707b64358433bcc4230c9505a2daca12465f
Axel Olsson <axel.olsson@gmail.com>
2018-04-16 09:30:20 +0200

    Ska gå att importera direkt i ts

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,2 +1,22 @@
 export * from './raa.module';
-export { RaaSelect } from './components/raa-select/raa-select.component';
+
+// Components
+import { RaaSelect } from './components/raa-select/raa-select.component';
+import { RaaDropdownComponent } from './components/raa-dropdown/raa-dropdown.component';
+import { RaaModalPageComponent } from './components/raa-modal-page/raa-modal-page.component';
+import { RaaDialogueComponent } from './components/raa-dialogue/raa-dialogue.component';
+import { RaaLabel } from './components/raa-label/raa-label.component';
+import { RaaTextWithSpinner } from './components/raa-text-with-spinner/raa-text-with-spinner.component';
+import { RaaSpinnerComponent } from './components/raa-spinner/raa-spinner.component';
+import { RaaOverlaySpinnerComponent } from './components/raa-overlay-spinner/raa-overlay-spinner.component';
+import { RaaPingServiceComponent } from './components/raa-ping-service/raa-ping-service.component';
+
+// Directives
+import { OutsideClickDirective } from './directives/raa-outside-click.directive';
+import { RaaRequestFocusDirective } from './directives/raa-request-focus.directive';
+import { RaaSelectAllOnFocus } from './directives/raa-select-all-on-focus.directive';
+import { RaaFocusInvalidElementDirective } from './directives/raa-focus-invalid-element.directive';
+import { RaaTrapFocusDirective } from './directives/raa-trap-focus.directive';
+
+// Pipes
+import { OrgnummerPipe } from './pipes/orgnummer.pipe';
