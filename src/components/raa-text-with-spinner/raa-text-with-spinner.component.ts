df9b01707798ec78e9e7d0687eaa8bfa5a15eb5d
Ivar Boson <ivar.boson@raa.se>
2019-12-13 16:19:01 +0100

    Styling fixar efter ny raa-style version

--- src/components/raa-text-with-spinner/raa-text-with-spinner.component.ts
+++ src/components/raa-text-with-spinner/raa-text-with-spinner.component.ts
@@ -2,12 +2,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'raa-text-with-spinner',
-  template: `<div class="tw-relative">
-        <div [ngClass]="{'tw-invisible': showSpinner}"><ng-content></ng-content></div>
-        <div class="tw-absolute tw-pin-t tw-pin-b tw-pin-r tw-pin-l" [ngClass]="{'tw-hidden': !showSpinner}">
-          <raa-spinner size="1rem"></raa-spinner>
-        </div>
-   </div>`,
+  template: `
+    <div class="tw-relative">
+      <div [ngClass]="{ 'tw-invisible': showSpinner }"><ng-content></ng-content></div>
+      <div class="tw-absolute tw-top-0 tw-bottom-0 tw-right-0 tw-left-0" [ngClass]="{ 'tw-hidden': !showSpinner }">
+        <raa-spinner size="1rem"></raa-spinner>
+      </div>
+    </div>
+  `,
 })
 export class RaaTextWithSpinner {
   @Input()
