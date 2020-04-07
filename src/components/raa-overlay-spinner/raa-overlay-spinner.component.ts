168200155f141358349886c67939b0a425e137b7
vizet <vizet@raa.se>
2020-04-07 14:36:29 +0200

    FOR-2120 fixat overlay-spinner och fixat utseende på navigation-bar så att den ser ut som i fornreg

--- src/components/raa-overlay-spinner/raa-overlay-spinner.component.ts
+++ src/components/raa-overlay-spinner/raa-overlay-spinner.component.ts
@@ -1,14 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'raa-overlay-spinner',
   templateUrl: './raa-overlay-spinner.component.html',
   styleUrls: ['./raa-overlay-spinner.component.scss'],
 })
-export class RaaOverlaySpinnerComponent {
+export class RaaOverlaySpinnerComponent implements AfterViewInit {
   @Input()
   posAbsolute: boolean;
 
   @Input()
   posFixed: boolean;
+
+  @ViewChild('raaSpinner', { static: true }) spinner: ElementRef;
+
+  private focusedElement: Element;
+
+  ngAfterViewInit() {
+    // Leta upp tidigare fokuserat element och spara en referens till den
+    // så att vi kan sätta tillbaka fokus på samma element när komponenten dör.
+    if (document.activeElement) {
+      this.focusedElement = document.activeElement;
+      (document.activeElement as HTMLElement).blur();
+    }
+
+    if (this.spinner) {
+      this.spinner.nativeElement.focus();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.focusedElement) {
+      (this.focusedElement as HTMLElement).focus();
+    }
+  }
 }
